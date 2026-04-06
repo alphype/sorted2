275b62aa0d350074b4294d41473529e46b5b3412
Taiwoo <alphype@gmail.com>
2026-04-06 11:51:09 +0100

    Neater names. Structured a little better.

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
+// List of variables. 
 let listOfNumbers = [];
 let stringOfNumbers; 
 let newListOfNumbers = [];
 
+// Add the event listener for the submit button. Everything happens after this button gets clicked. 
 submitButton.addEventListener("click", sortingFunction);
 
+// This is the function that gets called when the button gets clicked. 
+/* 
+Functionalities:
+    1. Removes any existing visualisation blocks. 
+    2. Reads input text field. 
+    3. Turns that information into an array of numbers. 
+    4. If the correct radio button is checked, run that sorting function, and the visualisation of that sorting function. 
+*/
 function sortingFunction() {
+    // Remove any existing canvases. 
+    let canvasToBeRemoved = document.getElementById("p5");
+    if (canvasToBeRemoved.hasChildNodes() == true) {
+        canvasToBeRemoved.removeChild(canvasToBeRemoved.lastChild);
+    }
+
+    //Read the value of the DOM object with ID "myInput". 
     let sortedList = myInput.value;
 
+    // Split the user-entered string into an array of strings for numbers. 
     listOfNumbers = sortedList.split(',');
 
+    // Reset the newListOfNumbers. 
     newListOfNumbers = [];
 
+    // For each element in the array of strings, convert them to a number and push them to the new array. 
     listOfNumbers.forEach(element => {
         newListOfNumbers.push(Number(element));
     });
 
-    let canvasToBeRemoved = document.getElementById("p5");
-
-    if (canvasToBeRemoved.hasChildNodes() == true) {
-        canvasToBeRemoved.removeChild(canvasToBeRemoved.lastChild);
-    }
-    
     if (document.getElementById("insertionSort").checked) {
         insertionSort(newListOfNumbers);   
         boxCreation(newListOfNumbers, newListOfNumbers.length);  
@@ -52,23 +66,47 @@ function sortingFunction() {
 
 }
 
+// My own insertion sort implementation. 
 function insertionSort(array) {
+    // Loop through the input array. 
     for (let i = 1; i < array.length; i++) {
-        let keyToCompare = array[i];
+
+        // Let the element to sort start from the second element in the array (since i == 1). 
+        let keyToSort = array[i];
         let j = i - 1;
-        for (; j > -1 && array[j] > keyToCompare; j--) {
+
+        // Loop through everything through the left of the key to sort. I could have used a `while` loop, but I like `for` loops better. 
+        // Stop looping when the key to sort is bigger than the j-element. 
+        for (; j > -1 && array[j] > keyToSort; j--) {
+            /* For every time the key to sort is smaller than the j-element, 
+            shift the left element to the right by one. */
             array[j + 1] = array[j];
         }
-        array[j + 1] = keyToCompare;
+
+        /* We are here when the j-element is smaller than the key to sort. So the key should go to the right of the j-element. 
+        This is permitted because array[j + 1] and array[j + 2] are the same, and so one needs to be replaced anyways.
+        If previous loop does not run (all the left elements are smaller than the key to sort), 
+        then the element stays in place!  */
+        array[j + 1] = keyToSort;
     }
     return array;
 }
 
+// My own bubble sort implementation. 
 function bubbleSort(array) {
+    /* Loop through the number of elements in the array
+    Note that this doesn't have to be the case. 
+    We can have a condition to break out of the loop, 
+    which would make best case scenario into n instead of n^2. 
+    Check time vs loop time */
     for (let i = 0; i < array.length; i++) {
+        // Loop through the array
         for (let j = 0; j < array.length; j++) {
+
             let firstToBeCompared = array[j];
             let secondToBeCompared = array[j + 1];
+
+            // Swap condition
             if (firstToBeCompared > secondToBeCompared) {
                 array[j + 1] = firstToBeCompared;
                 array[j] = secondToBeCompared;
@@ -78,28 +116,50 @@ function bubbleSort(array) {
     return array;
 }
 
+// My own selection sort implementation. 
 function selectionSort(array) {
+    // Define the new array for the selected elements to go in. 
     let newArray = [];
+
+    // Start from large loop
     for (let i = array.length - 1; i >= 0; i--) {
+        // Set smallest element as the element of i. 
         let smallestElementIndex = i;
+
+        // Loop upwards the length of the shortening array
+        // We don't want unnecessary looping 
         for (let j = 0; j < i; j++) {
+            // If there is a smaller element, make that index equal to smallestElementIndex. 
             if (array[smallestElementIndex] > array[j]) {
                 smallestElementIndex = j;
             }
         }
+
+        // After loop, remove smallest element from original array and add to new array. 
         newArray.push(array.splice(smallestElementIndex, 1)[0]);
     }
     return newArray;
 }
 
+// My own merge sort implementation. 
 function mergeSort(array) {
+    // Let the merged array == sorted. 
     let sorted = [];
+
+    // Condition for split and merge. 
     if (array.length > 1) {
+        // Define the sorted arrays. 
         let arrayOne = mergeSort(array.slice(0, array.length/2));
         let arrayTwo = mergeSort(array.slice(array.length/2, array.length));
+
+        // Indicies for each of the sorted arrays. 
         let j = 0;
         let k = 0;
+
+        // Loop through the amount of elements to be sorted. 
         for (let i = 0; i < arrayOne.length + arrayTwo.length; i++) {
+
+            // Compare between the two sorted arrays to merge into one sorted array
             if (arrayOne[j] >= arrayTwo[k] || arrayOne[j] == undefined) {
                 sorted.push(arrayTwo[k]);
                 k++;
@@ -108,39 +168,55 @@ function mergeSort(array) {
                 j++;
             }
         }
+
+        // return to continue recursion
         return sorted;
     } else {
+        // return for the bottom of recursion
         return array;
     }
 }
 
+// Displaying list of sorted numbers. Need to create boxes with each element in the sorted array. 
 function boxCreation(array, length) {
+
+    // Target the sorted list box
     let smallestBox = document.getElementById("smallest-box");
     
-    let id = document.getElementById("sortedLabel");
+    // Set the sorted list label as an achor for the rest of the box creation. 
+    let sortedLabel = document.getElementById("sorted-list-label");
 
+    // If there is only the list label, then create.
     if (smallestBox.childElementCount == 1) {
+        // Reverse loop because we want smallest element added at the end of the anchor
         for (let k = length - 1; k >= 0; k--) {
             let newBox = document.createElement("p");
             newBox.setAttribute("id", `${k}`);
+            newBox.setAttribute("class", "text");
             newBox.innerHTML = array[k].toString();
-            id.after(newBox); 
+
+            // Place after anchor
+            sortedLabel.after(newBox); 
         }
     } else {
+        // Remove existing boxes. 
         for (let k = smallestBox.childElementCount - 2; k >= 0; k--) {
             let toBeRemoved = document.getElementById(`${k}`);
             toBeRemoved.remove();
         }
 
+        // Add new boxes again. Refresh. 
         for (let k = length - 1; k >= 0; k--) {
             let newBox = document.createElement("p");
             newBox.setAttribute("id", `${k}`);
+            newBox.setAttribute("class", "text");
             newBox.innerHTML = array[k].toString();
-            id.after(newBox); 
+            sortedLabel.after(newBox); 
         }
     }
 }
 
+// The visualisation for insertion sort. 
 function insertionSortSketch(p) {
     let currentStep = 0;
     let sortedArray = insertionSort(newListOfNumbers);
@@ -191,6 +267,7 @@ function insertionSortSketch(p) {
 
 }
 
+// The visualisation for merge sort. 
 function mergeSortSketch(p) {
     let currentStep = 0;
 
@@ -303,6 +380,7 @@ function mergeSortSketch(p) {
     }
 }
 
+// The visualisation for bubble sort. 
 function bubbleSortSketch(p) {
     let currentStep = 0;
     let sortedArray = bubbleSort(newListOfNumbers);
@@ -354,6 +432,7 @@ function bubbleSortSketch(p) {
     }
 }
 
+// The incomplete visualisation for selection sort. 
 function selectionSortSketch(p) {
     newListOfNumbers = [];
 
@@ -416,6 +495,7 @@ function selectionSortSketch(p) {
     }
 }
 
+// Log function. 
 function getBaseLog(x, y) {
     return Math.log(y) / Math.log(x);
 }
